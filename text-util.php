a566f6af24234ca721c048833928c13df1ddb9f7
khatskelevich <khatskelevich@proitr.ru>
2021-01-13 13:20:30 +0500

    Add try-catch block

--- text-util.php
+++ text-util.php
@@ -4,36 +4,43 @@ spl_autoload_register(function ($class) {
     include 'app/' . $class . '.php';
 });
 
-if (count($argv) == 3) {
-    switch ($argv[1]) {
-        case 'comma':
-        case ',':
-            $csv = new ParseCSV('people.csv', ',');
-            break;
-        case 'semicolon':
-        case ':':
-            $csv = new ParseCSV('people.csv', ':');
-            break;
-        default:
-            exit('Невалидный первый аргумент. Используйте comma/semicolon');
-    }
+try {
+    if (count($argv) == 3) {
+        switch ($argv[1]) {
+            case 'comma':
+            case ',':
+                $csv = new ParseCSV('people.csv', ',');
+                break;
+            case 'semicolon':
+            case ':':
+                $csv = new ParseCSV('people.csv', ':');
+                break;
+            default:
+                throw new Exception('Невалидный первый аргумент. Используйте comma/semicolon');
+        }
 
-    $files = new Files('texts');
+        $files = new Files('texts');
 
-    switch ($argv[2]) {
-        case 'countAverageLineCount':
-            countAverageLineCount($csv->getUsers(), $files);
-            break;
-        case 'replaceDates':
-            $files->replaceDates(basicFormat: 'd/m/y', destinationFormat: 'm-d-y');
-            break;
-        default:
-            exit('Невалидный второй аргумент: ' . $argv[2] . ' Используйте countAverageLineCount/replaceDates');
-    }
+        switch ($argv[2]) {
+            case 'countAverageLineCount':
+                countAverageLineCount($csv->getUsers(), $files);
+                break;
+            case 'replaceDates':
+                $files->replaceDates(basicFormat: 'd/m/y', destinationFormat: 'm-d-y');
+                break;
+            default:
+                throw new Exception('Невалидный второй аргумент: ' . $argv[2] . ' Используйте countAverageLineCount/replaceDates');
+        }
+    } else throw new Exception('Некорректное количество аргументов');
+} catch (Exception $e) {
+    exit($e->getMessage());
+}
 
-
-} else exit('Некорректное количество аргументов');
-
+/**
+ * @param $users
+ * @param Files $files
+ * @throws Exception
+ */
 function countAverageLineCount($users, Files $files)
 {
     echo "\nСреднее количество строк в тексте для каждого пользователя\n\n";
