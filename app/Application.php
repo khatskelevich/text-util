358666ba263028d9e2320ad2e026b4bd5a8013b2
khatskelevich <khatskelevich@proitr.ru>
2021-01-21 00:45:17 +0500

    Add Application class. Now it's more OOP.

---
+++ app/Application.php
@@ -0,0 +1,64 @@
+<?php
+
+
+class Application
+{
+    private $method;
+    private $delimiter;
+    private $dirname = 'texts';
+
+    public function __construct($argv)
+    {
+        if (count($argv) >= 3) {
+            $this->delimiter = $argv[1];
+            $this->method = $argv[2];
+        } else exit('Некорректное количество аргументов');
+    }
+
+    public function start()
+    {
+        try {
+            switch ($this->method) {
+                case 'countAverageLineCount':
+                    $this->countAverageLineCount();
+                    break;
+                case 'replaceDates':
+                    $this->replaceDates();
+                    break;
+                default:
+                    throw new Exception('Невалидный второй аргумент: ' . $this->method . ' Используйте countAverageLineCount/replaceDates');
+            }
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function countAverageLineCount()
+    {
+        $csv = match ($this->delimiter) {
+            'comma', ',' => new ParseCSV('people.csv', ','),
+            'semicolon', ':' => new ParseCSV('people.csv', ':'),
+            default => throw new Exception('Невалидный первый аргумент. Используйте comma/semicolon'),
+        };
+        $users = $csv->getUsers();
+
+        $files = new Files($this->dirname);
+
+        echo "\nСреднее количество строк в тексте для каждого пользователя\n\n";
+        foreach ($users as $user) {
+            echo $user['name'] . ': ' . $files->averageLineCount($user['id']) . "\n";
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function replaceDates(){
+        $files = new Files($this->dirname);
+
+        $files->replaceDates(basicFormat: 'd/m/y', destinationFormat: 'm-d-y');
+    }
+}
--- text-util.php
+++ text-util.php
@@ -4,40 +4,5 @@ spl_autoload_register(function ($class) {
     include 'app/' . $class . '.php';
 });
 
-try {
-    if (count($argv) == 3) {
-        $csv = match ($argv[1]) {
-            'comma', ',' => new ParseCSV('people.csv', ','),
-            'semicolon', ':' => new ParseCSV('people.csv', ':'),
-            default => throw new Exception('Невалидный первый аргумент. Используйте comma/semicolon'),
-        };
-
-        $files = new Files('texts');
-
-        switch ($argv[2]) {
-            case 'countAverageLineCount':
-                countAverageLineCount($csv->getUsers(), $files);
-                break;
-            case 'replaceDates':
-                $files->replaceDates(basicFormat: 'd/m/y', destinationFormat: 'm-d-y');
-                break;
-            default:
-                throw new Exception('Невалидный второй аргумент: ' . $argv[2] . ' Используйте countAverageLineCount/replaceDates');
-        }
-    } else throw new Exception('Некорректное количество аргументов');
-} catch (Exception $e) {
-    exit($e->getMessage());
-}
-
-/**
- * @param $users
- * @param Files $files
- * @throws Exception
- */
-function countAverageLineCount($users, Files $files)
-{
-    echo "\nСреднее количество строк в тексте для каждого пользователя\n\n";
-    foreach ($users as $user) {
-        echo $user['name'] . ': ' . $files->averageLineCount($user['id']) . "\n";
-    }
-}
+$app = new Application($argv);
+$app->start();
